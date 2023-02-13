6c7ba055a75b42da08e57e5a51f30096a1101bdc
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-02-13 14:03:19 +0100

    add Test d'ajout

--- Assets/ColdRessources/Scripts/GameManager.cs
+++ Assets/ColdRessources/Scripts/GameManager.cs
@@ -2,14 +2,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 	public PlayerMouvements player;
+    public GameObject point;
 
-	void FixedUpdate() {
-		if (Input.touchCount > 0) {
-			Touch touch = Input.GetTouch(0);
-			if (touch.phase == TouchPhase.Moved) {
-				Vector3 touchGap = touch.deltaPosition.normalized * Time.deltaTime;
-				player.MoveBy(touchGap);
-			}
-		}
-	}
+    private Vector2 startTouchPosition;
+    private Vector2 currentTouchPosition;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTouchPosition = touch.position.normalized;
+                point.transform.position = new Vector3(startTouchPosition.x, startTouchPosition.y, 1);
+                    
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                
+                point.transform.position = new Vector3(0, 0, 10);
+
+            }
+            /*if (touch.phase == TouchPhase.Moved)*/
+            {
+                currentTouchPosition = touch.position.normalized;
+                if (touch.position.x < 0) currentTouchPosition.x *= -1;
+                if (touch.position.y < 0) currentTouchPosition.x *= -1;
+                Vector2 touchGap = currentTouchPosition - startTouchPosition;
+
+                player.MoveBy(touchGap*Time.deltaTime);
+            }
+        }
+    }
 }
--- Assets/Ressources/Character/Controller/PlayerMouvements.cs
+++ Assets/Ressources/Character/Controller/PlayerMouvements.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
 public class PlayerMouvements : MonoBehaviour {
-	public Vector3 moveSpeed = new Vector3(25f, 30f, 0f);
+	public Vector3 moveSpeed = new Vector3(25f, 25f, 0f);
+    public Vector3 pubic;
 
-	public void MoveBy(Vector3 move) {
-		transform.position += Vector3.Scale(move, moveSpeed);
+    public void MoveBy(Vector3 move) {
+        pubic = new Vector3(move.x * moveSpeed.x, move.y * moveSpeed.y, 0f);
+        transform.position += pubic;
 	}
 }
