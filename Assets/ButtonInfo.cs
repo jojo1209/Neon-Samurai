aaa15e45a7aeb1cd540ace9dcd7df66ab5cc539a
JulesGilli <jules.gilli@live.fr>
2023-03-13 18:53:36 +0100

    Shop finish

---
+++ Assets/ButtonInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int ItemID;
+    public TMP_Text PriceTxt;
+    public TMP_Text QuantityTxt;
+    public GameObject ShopManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PriceTxt.text = "Price: " + ShopManager.GetComponent<ShopManager>().shopItem[2,ItemID].ToString();
+        QuantityTxt.text = ShopManager.GetComponent<ShopManager>().shopItem[3, ItemID].ToString();
+    }
+}
---
+++ Assets/ShopManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ShopManager : MonoBehaviour
+{
+
+    public int[,] shopItem = new int[5, 5];
+    public float coins;
+    public TMP_Text CoinsTXT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoinsTXT.text = "Coins: " + coins.ToString();
+
+        //ID
+        shopItem[1, 1] = 1;
+        shopItem[1, 2] = 2;
+        shopItem[1, 3] = 3;
+
+        //Price
+        shopItem[2, 1] = 10;
+        shopItem[2, 2] = 20;
+        shopItem[2, 3] = 30;
+
+        //Quantity
+        shopItem[3, 1] = 0;
+        shopItem[3, 2] = 0;
+        shopItem[3, 3] = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Buy() 
+    {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+
+        if (coins >= shopItem[2, ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        {
+            coins -= shopItem[2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
+            shopItem[3, ButtonRef.GetComponent<ButtonInfo>().ItemID]++;
+            CoinsTXT.text = "Coins: " + coins.ToString();
+            ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text = shopItem[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
+        }
+
+    }
+
+}
