d2828814695584a5e173f5683e01bbc6b9e42aa9
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-15 17:51:33 +0100

    Moved stuff to the rightfull folder

---
+++ Assets/ColdRessources/Scripts/GameUI/BulletTimeButton.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class BulletTimeButton: MonoBehaviour
+{
+	[SerializeField] private Sprite activable;
+	[SerializeField] private Sprite nonActivable;
+	[SerializeField] private AttackPlayer attackPlayer;
+	[SerializeField] private float cooldown;
+	private Image image;
+	private 
+
+	private void Start()
+	{
+		image = GetComponent<Image>();
+	}
+	
+	Update
+	
+}
