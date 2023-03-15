2f53139972dbae30b6c852dd3ee22c203a9cead2
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-15 13:02:15 +0100

    Update GameManager.cs

--- Assets/ColdRessources/Scripts/GameManager.cs
+++ Assets/ColdRessources/Scripts/GameManager.cs
@@ -1,16 +1,7 @@
-using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public static GameManager Instance;
-	
-	public void Awake()
-	{
-		if (Instance == null)
-			Instance = this;
-	}
-
 	private void Start() {
 		Time.timeScale = 1;
 	}
