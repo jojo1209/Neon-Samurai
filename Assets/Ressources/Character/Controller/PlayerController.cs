febfa977eb907053fd69a26306c1b329f194a0cc
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-15 12:49:15 +0100

    Update PlayerController.cs

--- Assets/Ressources/Character/Controller/PlayerController.cs
+++ Assets/Ressources/Character/Controller/PlayerController.cs
@@ -1,32 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController: MonoBehaviour
 {
-public FixedJoystick joystick;
-public Rigidbody myRigidbody;
-public float speed = 20f;
+	public FixedJoystick joystick;
+	public Rigidbody myRigidbody;
+	public float speed = 20f;
 
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        myRigidbody.velocity = new Vector3(joystick.Horizontal * speed, joystick.Vertical * speed, myRigidbody.velocity.z);
-
-        
-    }
+	private void Update()
+	{
+		myRigidbody.velocity = new Vector3(
+			joystick.Horizontal * speed,
+			joystick.Vertical * speed,
+			myRigidbody.velocity.z);
+	}
 }
-
-
-
-
-// if (Input.touchCount > 0)
-//         {
-//             Touch touch = Input.GetTouch(0);
-//             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-//             touchPosition.z = 0f;
-//             transform.position = touchPosition;
-//         }
