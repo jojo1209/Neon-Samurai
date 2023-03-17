059c9ec21f3cad1ac537153ec1ffe26edd7474b5
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-03-17 08:39:43 +0100

    Update PlayerAnimation.cs
    - inverted ifs statements for a cleaner code
    - explicitelly declared variables private

--- Assets/Ressources/Character/Controller/PlayerAnimation.cs
+++ Assets/Ressources/Character/Controller/PlayerAnimation.cs
@@ -1,37 +1,32 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlayerAnimation : MonoBehaviour
+public class PlayerAnimation: MonoBehaviour
 {
-   public Animator playerAnim;
-   private static readonly int IsAttacking = Animator.StringToHash("IsAttacking");
-   private static readonly int IsDead = Animator.StringToHash("IsDead");
-    public GameObject vfx;
-    bool goVFX;
-    float time = 0;
-    public void Update()
-    {
-        if (goVFX)
-        {
-            time += Time.deltaTime;
-            vfx.SetActive(true);
+	public Animator playerAnim;
+	private static readonly int IsAttacking = Animator.StringToHash("IsAttacking");
+	private static readonly int IsDead = Animator.StringToHash("IsDead");
+	public GameObject vfx;
+	private bool goVFX;
+	private float time = 0;
+	public void Update()
+	{
+		if (!goVFX) return;
+		time += Time.deltaTime;
+		vfx.SetActive(true);
 
-            if (time >= 0.5f)
-            {
-                vfx.SetActive(false);
-                goVFX= false;
-            }
-        }
-
-    }
-    public void OnAttack()
-   {
-        time = 0;
-        goVFX= true;
-      playerAnim.SetTrigger(IsAttacking);
-   }
-   public void OnDead()
-   {
-      playerAnim.SetTrigger(IsDead);
-   }
+		if (!(time >= 0.5f)) return;
+		vfx.SetActive(false);
+		goVFX = false;
+	}
+	public void OnAttack()
+	{
+		time = 0;
+		goVFX = true;
+		playerAnim.SetTrigger(IsAttacking);
+	}
+	public void OnDead()
+	{
+		playerAnim.SetTrigger(IsDead);
+	}
 }
